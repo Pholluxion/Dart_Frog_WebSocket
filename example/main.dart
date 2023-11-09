0a95c9253e7c95e0a52745709a51aaeedcdfb120
Pholluxion <penalozadaniel00@gmail.com>
2023-11-09 18:40:05 -0500

    update

--- example/main.dart
+++ example/main.dart
@@ -1,7 +1,10 @@
 import 'package:web_socket_channel/web_socket_channel.dart';
 
 void main() {
-  final String url = String.fromEnvironment('URL');
+  final String url = String.fromEnvironment(
+    'URL',
+    defaultValue: "viaduct.proxy.rlwy.net:44691",
+  );
 
   final uri = Uri.parse('ws://${url}/ws');
   final channel = WebSocketChannel.connect(uri);
